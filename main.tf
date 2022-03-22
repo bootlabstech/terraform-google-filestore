eba15ec3a1f457c158ebeb23a61a7f43c55b15da
Silpashree <silpashree@bootlabstech.com>
2022-03-22 14:08:56 +0530

    initial commit

---
+++ main.tf
@@ -0,0 +1,14 @@
+resource "google_filestore_instance" "instance" {
+  name    = var.name
+  zone    = var.zone
+  tier    = var.tier
+  project = var.project_id
+  file_shares {
+    capacity_gb = var.capacity_gb
+    name        = var.file_shares_name
+  }
+  networks {
+    network = var.network
+    modes   = var.modes
+  }
+}
