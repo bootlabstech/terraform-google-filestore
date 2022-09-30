edc3dbdee91f6dde221586cc48037d114e307a04
Silpashree <silpashree@bootlabstech.com>
2022-09-30 13:32:19 +0530

    fix/provider&api

--- main.tf
+++ main.tf
@@ -1,16 +1,21 @@
-resource "google_filestore_instance" "instance" {
-  name    = var.name
-  zone    = var.zone
-  tier    = var.tier
+resource "google_project_service" "filestoreapi" {
   project = var.project_id
+  service = "file.googleapis.com"
+}
+resource "google_filestore_instance" "instance" {
+  depends_on = [google_project_service.filestoreapi]
+  name       = var.name
+  zone       = var.zone
+  tier       = var.tier
+  project    = var.project_id
   file_shares {
     capacity_gb = var.capacity_gb
     name        = var.file_shares_name
   }
   networks {
-    network = var.network
-    modes   = var.modes
+    network           = var.network
+    modes             = var.modes
     reserved_ip_range = var.reserved_ip_range
-    connect_mode = var.connect_mode
+    connect_mode      = var.connect_mode
   }
 }
